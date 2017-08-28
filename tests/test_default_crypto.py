c9171294ed1da19e872cc83956c22eed2fa8af5c
jMyles <justin@justinholmes.com>
2017-08-28 14:24:06 -0700

    Shows error in pre.reencrypt trying to recreate ephemeral scenario from whitepaper.

--- tests/test_default_crypto.py
+++ tests/test_default_crypto.py
@@ -15,7 +15,7 @@ def test_symmetric():
     assert cipher.decrypt(edata) == data
 
 
-def test_pre():
+def test_pre_rekey_with_private_and_public():
     pre = pre_from_algorithm(default_algorithm)
 
     sk_alice = b'a' * 32
@@ -36,3 +36,36 @@ def test_pre():
     cyphertext_for_bob = pre.reencrypt(rk_alice_bob, cyphertext_for_alice)
     # ...and sure enough, Bob can read it!
     assert pre.decrypt(sk_bob, cyphertext_for_bob) == cleartext
+
+
+def test_pre_rekey_with_private_and_ephemeral():
+    """
+    Demonstration of Chapter II, Section A, paragraph 2, lines 10-15 of whitepaper
+    """
+    cleartext = "A Mr. Death or something - he's come about the reaping."
+    pre = pre_from_algorithm(default_algorithm)
+    key_length = 32
+
+    sk_alice = b'a' * key_length
+    pk_alice = pre.priv2pub(sk_alice)
+
+    sk_bob = b'b' * 32
+    pk_bob = pre.priv2pub(sk_bob)
+
+    sk_ephemeral = crypto.random(key_length)
+    pk_ephemeral = pre.priv2pub(sk_ephemeral)
+
+    rk_alice_to_ephemeral = pre.rekey(sk_alice, sk_ephemeral)
+
+    sk_ephemeral_encrypted = pre.encrypt(pk_bob, sk_ephemeral)
+
+    rk_alice_to_bob = pre.rekey(rk_alice_to_ephemeral, sk_ephemeral_encrypted)
+
+    # re-encryption node activity
+
+    cyphertext_for_alice = pre.encrypt(pk_alice, cleartext)
+    cyphertext_ephemeral = pre.reencrypt(rk_alice_to_ephemeral, cyphertext_for_alice)
+    cyphertext_for_bob = pre.reencrypt(cyphertext_ephemeral, sk_ephemeral_encrypted)
+
+    # decryption by bob
+    bob_possession_sk_ephemeral_encrypted = pre.decrypt(pk_ephemeral, sk_bob, )
