0ea54fec717fd7aa23aaa584adb9e3a5319d2218
Michael Egorov <michwill@gmail.com>
2019-10-02 22:48:17 -0700

    Make PEP8 happy

--- nucypher/characters/lawful.py
+++ nucypher/characters/lawful.py
@@ -720,7 +720,6 @@ class Bob(Character):
                 else:
                     self.log.warn("Found existing complete WorkOrders, but use_precedent_work_orders is set to False.  To use Bob in 'KMS mode', set retain_cfrags=False as well.")
 
-
         # Part II: Getting the cleartexts.
         cleartexts = []
 
