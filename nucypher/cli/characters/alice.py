713e3af8870327f1d1ec75ed80bcf49f37c61db8
damon <gdamon@gmail.com>
2020-01-10 12:35:00 -0800

    rename 'connect' to 'connect_cli'

--- nucypher/cli/characters/alice.py
+++ nucypher/cli/characters/alice.py
@@ -304,7 +304,7 @@ def run(general_config, character_options, config_file, controller_port, dry_run
 
 
 @alice.command("public-keys")
-@AliceInterface.connect('public_keys')
+@AliceInterface.connect_cli('public_keys')
 @group_character_options
 @option_config_file
 @group_general_config
@@ -319,7 +319,7 @@ def public_keys(general_config, character_options, config_file):
 
 
 @alice.command('derive-policy-pubkey')
-@AliceInterface.connect('derive_policy_encrypting_key')
+@AliceInterface.connect_cli('derive_policy_encrypting_key')
 @group_character_options
 @option_config_file
 @group_general_config
@@ -334,7 +334,7 @@ def derive_policy_pubkey(general_config, label, character_options, config_file):
 
 
 @alice.command()
-@AliceInterface.connect('grant')
+@AliceInterface.connect_cli('grant')
 @group_character_options
 @option_config_file
 @group_general_config
@@ -372,7 +372,7 @@ def grant(general_config,
 
 
 @alice.command()
-@AliceInterface.connect('revoke')
+@AliceInterface.connect_cli('revoke')
 @group_character_options
 @option_config_file
 @group_general_config
@@ -398,7 +398,7 @@ def revoke(general_config,
 
 
 @alice.command()
-@AliceInterface.connect('decrypt')
+@AliceInterface.connect_cli('decrypt')
 @group_character_options
 @option_config_file
 @group_general_config
